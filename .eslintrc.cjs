9dc1ea6641b874eabb6b1f8389d7a35c4309f23c
José Teles <josetelesmaciel@gmail.com>
2024-08-01 14:13:05 -0300

    chore: atualiza import/no-unresolved para ignorar caminhos com "~/"

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,7 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "react/react-in-jsx-scope": "off",
     "import/no-named-as-default": 0,
-    "import/no-unresolved": ["error", { ignore: ["^@/"] }],
+    "import/no-unresolved": ["error", { ignore: ["^~/"] }],
     "react/display-name": "off",
     "@typescript-eslint/ban-types": "off",
     "import/named": "off",
