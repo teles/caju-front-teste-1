37b6361ef43dffc4e46131dd1ff0f35219eec685
José Teles <josetelesmaciel@gmail.com>
2024-08-04 18:20:33 -0300

    docs: adiciona docs para utils (sem issue)

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -1,1 +1,7 @@
+/**
+ * Remove todos os caracteres não numéricos de uma string.
+ *
+ * @param text - A string da qual os caracteres não numéricos devem ser removidos.
+ * @returns A string resultante após a remoção dos caracteres não numéricos.
+ */
 export const removeNonNumeric = (text: string) => text.replace(/\D/g, "");
--- src/utils/toastUtils.ts
+++ src/utils/toastUtils.ts
@@ -1,6 +1,10 @@
 import { toast } from "react-toastify";
 import { ActionResponse } from "~/types/actionResponse";
 
+/**
+ * Exibe um toast de sucesso ou erro com base na resposta da ação.
+ * @param actionResponse - A resposta da ação.
+ */
 export const showToast = (actionResponse: ActionResponse) => {
   if (actionResponse.success) {
     toast.success(actionResponse.message);
